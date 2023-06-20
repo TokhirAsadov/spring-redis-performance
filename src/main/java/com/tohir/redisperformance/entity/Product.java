5c578ac5c90339700a78971d46ffc361a5326ce7
Bismillah <guvalakat1603@gmail.con>
2023-06-20 16:06:49 +0500

    add lombok items

--- src/main/java/com/tohir/redisperformance/entity/Product.java
+++ src/main/java/com/tohir/redisperformance/entity/Product.java
@@ -1,11 +1,15 @@
 package com.tohir.redisperformance.entity;
 
+import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.springframework.data.annotation.Id;
 
 @Data
 @ToString
+@AllArgsConstructor
+@NoArgsConstructor
 public class Product {
 
     @Id
